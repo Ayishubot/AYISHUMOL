7a96f4e3786b6d95359f023dddceb4a052698aad
Ayishubot <100479004+Ayishubot@users.noreply.github.com>
2022-02-26 20:30:15 +0530

    Update pinky.js

--- pinky.js
+++ pinky.js
@@ -1,4 +1,4 @@
-/* codded by afnanplk
+/* codded by ꜰᴀꜱɪʟ
 */
 
 const { Sequelize } = require('sequelize');
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
+ PHONE: process.env.NUMBER === undefined ? '+919947430792' : process.env.NUMBER,   
  OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER,    
 };
